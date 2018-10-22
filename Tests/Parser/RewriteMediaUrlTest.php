333f41c9124c6dfe151f4ec297e9ab2672330752
Georgos Landin <Georgos.Landin@hotmail.com>
2018-10-22 13:48:59 +0200

    unsure if we want this test

--- Tests/Parser/RewriteMediaUrlTest.php
+++ Tests/Parser/RewriteMediaUrlTest.php
@@ -22,13 +22,12 @@ class RewriteMediaUrlTest extends TestCase
 
         $media = $this->getMockBuilder(Media::class)
             ->getMock();
+
         $media->expects($this->once())
             ->method('setSourceUrl');
 
         $parser = new RewriteMediaUrl('http://wordpress.com/wp-conent/uploads/2018/foobar.jpg', $router);
         $parser->parseMedia($media);
-
-        $media->method('getSourceUrl')->willReturn($outputUrl);
     }
 
     public function urlProvider()
